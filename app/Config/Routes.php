817f6b81ec4c09ba508262e8fd4710a76813add5
Dharma Situmorang <dharmabakti1202@gmail.com>
2025-12-23 09:42:52 +0700

    Refactor user routes to use UserController instead of BerandaController

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,8 +3,8 @@
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\Auth\LoginController;
 use App\Controllers\DashboardController;
-use App\Controllers\BerandaController;
 use App\Controllers\MenuController;
+use App\Controllers\UserController;
 
 /**
  * @var RouteCollection $routes
@@ -28,15 +28,15 @@ $routes->get('/refresh', [LoginController::class, 'refreshToken'], ['as' => 'tok
 $routes->group('', ['filter' => 'authenticated', 'csrf' => true], function ($routes) {
     $routes->get('dashboard', [DashboardController::class, 'index'], ['as' => 'dashboard.index']);
 
-    $routes->get('/users', [BerandaController::class, 'index'], ['as' => 'users.index']);
-    $routes->get('/users/getUsers', [BerandaController::class, 'getUsers'], ['as' => 'users.getUsers']);
-    $routes->post('/users/createUser', [BerandaController::class, 'createUser'], ['as' => 'users.createUser']);
-    // $routes->post('/users/updateUser/:id', [BerandaController::class, 'updateUser'], ['as' => 'users.updateUser']);
-    // $routes->post('/users/deleteUser/:id', [BerandaController::class, 'deleteUser'], ['as' => 'users.deleteUser']);
+    $routes->get('/users', [UserController::class, 'index'], ['as' => 'users.index']);
+    $routes->get('/users/getUsers', [UserController::class, 'getUsers'], ['as' => 'users.getUsers']);
+    $routes->post('/users/createUser', [UserController::class, 'createUser'], ['as' => 'users.createUser']);
+    // $routes->post('/users/updateUser/:id', [UserController::class, 'updateUser'], ['as' => 'users.updateUser']);
+    // $routes->post('/users/deleteUser/:id', [UserController::class, 'deleteUser'], ['as' => 'users.deleteUser']);
 
     $routes->get('/menu', [MenuController::class, 'index'], ['as' => 'menu.index']);
 
 
-    $routes->get('/about', [BerandaController::class, 'about'], ['as' => 'about.index']);
-    $routes->get('/contact', [BerandaController::class, 'contact'], ['as' => 'contact.index']);
+    // $routes->get('/about', [BerandaController::class, 'about'], ['as' => 'about.index']);
+    // $routes->get('/contact', [BerandaController::class, 'contact'], ['as' => 'contact.index']);
 });
