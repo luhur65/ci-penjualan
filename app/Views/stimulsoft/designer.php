12af25c60ed4e0ae860c1535a9fe45b7ddff720e
Dharma Situmorang <dharmabakti1202@gmail.com>
2026-07-16 14:38:34 +0700

    Feat: Add Stimulsoft viewer and designer test pages

---
+++ app/Views/stimulsoft/designer.php
@@ -0,0 +1,42 @@
+<?= $this->extend('layouts/app') ?>
+
+<?= $this->section('content') ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Stimulsoft Designer Test</h3>
+                </div>
+                <div class="card-body">
+                    <div id="designerContent"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection() ?>
+
+<?= $this->section('scripts') ?>
+<link href="<?= base_url('public/libraries/stimulsoft/Css/stimulsoft.viewer.office2013.whiteblue.css') ?>" rel="stylesheet">
+<link href="<?= base_url('public/libraries/stimulsoft/Css/stimulsoft.designer.office2013.whiteblue.css') ?>" rel="stylesheet">
+
+<script src="<?= base_url('public/libraries/stimulsoft/Scripts/stimulsoft.reports.js') ?>" type="text/javascript"></script>
+<script src="<?= base_url('public/libraries/stimulsoft/Scripts/stimulsoft.viewer.js') ?>" type="text/javascript"></script>
+<script src="<?= base_url('public/libraries/stimulsoft/Scripts/stimulsoft.designer.js') ?>" type="text/javascript"></script>
+
+<script type="text/javascript">
+    var options = new Stimulsoft.Designer.StiDesignerOptions();
+    options.appearance.fullScreenMode = false; 
+
+    var designer = new Stimulsoft.Designer.StiDesigner(options, "StiDesigner", false);
+
+    // Bikin report kosong untuk ngetes license key
+    var report = new Stimulsoft.Report.StiReport();
+    report.createNewReport();
+    
+    // Assign report to designer
+    designer.report = report;
+    designer.renderHtml("designerContent");
+</script>
+<?= $this->endSection() ?>
---
+++ app/Views/stimulsoft/viewer.php
@@ -0,0 +1,37 @@
+<?= $this->extend('layouts/app') ?>
+
+<?= $this->section('content') ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Stimulsoft Viewer Test</h3>
+                </div>
+                <div class="card-body">
+                    <div id="viewerContent"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection() ?>
+
+<?= $this->section('scripts') ?>
+<link href="<?= base_url('public/libraries/stimulsoft/Css/stimulsoft.viewer.office2013.whiteblue.css') ?>" rel="stylesheet">
+<script src="<?= base_url('public/libraries/stimulsoft/Scripts/stimulsoft.reports.js') ?>" type="text/javascript"></script>
+<script src="<?= base_url('public/libraries/stimulsoft/Scripts/stimulsoft.viewer.js') ?>" type="text/javascript"></script>
+
+<script type="text/javascript">
+    var options = new Stimulsoft.Viewer.StiViewerOptions();
+    var viewer = new Stimulsoft.Viewer.StiViewer(options, "StiViewer", false);
+
+    // Bikin report kosong untuk ngetes license key
+    var report = new Stimulsoft.Report.StiReport();
+    report.createNewReport();
+    
+    // Assign report to viewer
+    viewer.report = report;
+    viewer.renderHtml("viewerContent");
+</script>
+<?= $this->endSection() ?>
